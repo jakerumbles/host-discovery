5b92141303d8647e2844d22b3e8f1463deb18dca
Jake Edwards <edwardsjake@live.com>
2019-02-15 14:34:27 -0600

    Getting there

---
+++ edwardsja_host_discovery.py
@@ -0,0 +1,39 @@
+#Name: Jake Edwards
+#Date: 2/14/2019
+#Professor: Dr. Glendowne
+#Class: CSC 435-001
+
+import socket
+import ipaddress
+import sys
+
+port = 55123 #random high level port
+
+choice = int(input("Enter 0 to search a network or Enter 1 to search a specific address: "))
+if choice == 0: #Network
+    CIDR = input("Enter a CIDR (ex. 123.123.123.123/16): ")
+    network = ipaddress.ip_network(CIDR)
+    print(network.num_addresses)
+
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as client:
+        for addr in network.hosts():
+            client.connect((str(addr), port))
+            msg = "Jake was here"
+            client.send(msg.encode())
+
+elif choice == 1: #Specific Address
+    IPaddr = input("Enter IP address (ex. 45.33.32.156): ")
+
+    #Create socket
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as client:
+        #Attempt to connect
+        try:
+            client.connect((IPaddr, port))
+        except Exception as e:
+            print(e)
+            print("Program closing...")
+            sys.exit()
+
+        msg = "Test message from Jake"
+        client.send(msg.encode())
+        print("Message sent")
